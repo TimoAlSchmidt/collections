ed77e3fee9c577552f8985e0aa3aed1e47e007f3
TimoSchmidtt <timos.minilaptop@gmail.com>
2022-02-23 19:20:54 +0100

    dict

--- mm.py
+++ mm.py
@@ -3,14 +3,16 @@ import random
 kleur = ['oranje', 'blauw', 'groen', 'bruin']
 
 random.seed()
-
+#"oranje": 0, "blauw": 0, "groen" : 0, "bruin" : 0
 def vulKleuren(amount):
-    zak = []
+    zak = {}
     for i in range(amount):
-        zak.append(kleur[random.randint(0,3)])
-
+        woord = kleur[random.randint(0,3)]
+        try:
+            zak[woord] += 1
+        except:
+            zak.update({woord : 1})
     return zak
-
 
 amount = int(input("Hoeveel kleuren?\n"))
 print(vulKleuren(amount))
